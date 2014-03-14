27a84d6df15696e057137cff7a0312670849b1f3
pxhpjx <pxhpjx@163.com>
2014-03-14 09:38:01 +0800

    Create

---
+++ DeleteYourFamily/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace DeleteYourFamily
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(13, 13);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(442, 21);
+            this.txtPath.TabIndex = 1;
+            this.txtPath.Text = "E:\\TTFundInterface\\Interface\\FundTrade.API";
+            // 
+            // btnDel
+            // 
+            this.btnDel.Location = new System.Drawing.Point(180, 40);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(75, 23);
+            this.btnDel.TabIndex = 2;
+            this.btnDel.Text = "Delete";
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.btnDel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 69);
+            this.Controls.Add(this.btnDel);
+            this.Controls.Add(this.txtPath);
+            this.Name = "Form1";
+            this.Text = "不听话删你全家";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnDel;
+    }
+}
+
---
+++ DeleteYourFamily/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace DeleteYourFamily
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            string[] Dirs = Directory.GetDirectories(txtPath.Text, "bin", System.IO.SearchOption.AllDirectories);
+            List<string> DirList = new List<string>();
+            foreach (string s in Dirs)
+            {
+                try
+                {
+                    Directory.Delete(s, true);
+                    Directory.Delete(s.Replace("bin", "obj"), true);
+                }
+                catch { }
+            }
+        }
+    }
+}
